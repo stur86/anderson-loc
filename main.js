bfa0380279464c2833a61800b8ce6238c456e68d
stur86 <simonesturniolo@gmail.com>
2019-04-22 12:41:49 +0100

    Sites generation

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+function mod(x, size) {
+    x = x < 0? x + Math.ceil(Math.abs(x)/size)*size : x;
+    return x%size;
+}
+
 function AndersonModel(container) {
     this.cont = d3.select(container);
 
@@ -6,6 +11,45 @@ function AndersonModel(container) {
     this.omegaAvg = 5.0;
     this.omegaSpread = 2.0;
 
+    this.init();
+}
+
+function Site() {
+    this.x = 0;
+    this.v = 0;
+    this.k = 0;
+    this.neighs = [];
+}
+
+AndersonModel.prototype = {
+    init: function() {
+        // Initialise model
+        this.csize = 100.0/(this.size-1)*0.2;
+        // Create nodes
+        var matrix = [];
+        for (var i = 0; i < this.size; ++i) {
+            matrix.push([]);
+            for (var j = 0; j < this.size; ++j) {
+                var s = new Site();
+                matrix[i].push(s);
+            }
+        }
+
+        // Now assign the neighbours and frequencies
+        var dn = [[-1, -1], [-1, 1], [1, -1], [1, 1]];
+        this.sites = [];
+        for (var i = 0; i < this.size; ++i) {
+            for (var j = 0; j < this.size; ++j) {
+                var n = matrix[i][j];
+                for (var k = 0; k < 4; ++k) {
+                    var i2 = mod(i + dn[k][0], this.size);
+                    var j2 = mod(j + dn[k][1], this.size);
+                    n.neighs.push(matrix[i2][j2]);
+                }
+                this.sites.push(n);
+            }
+        }
+    }
 }
 
 var model = new AndersonModel();
