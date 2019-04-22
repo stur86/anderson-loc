6b6fd83703af7c1dacc945595cc291754942cc70
stur86 <simonesturniolo@gmail.com>
2019-04-22 13:05:51 +0100

    Added drawing

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function mod(x, size) {
-    x = x < 0? x + Math.ceil(Math.abs(x)/size)*size : x;
-    return x%size;
+    x = x < 0 ? x + Math.ceil(Math.abs(x) / size) * size : x;
+    return x % size;
 }
 
 function AndersonModel(container) {
@@ -11,26 +11,38 @@ function AndersonModel(container) {
     this.omegaAvg = 5.0;
     this.omegaSpread = 2.0;
 
+    this.cscale = chroma.scale(['yellow', 'navy']).mode('lch');
+
     this.init();
 }
 
-function Site() {
+function Site(i, j) {
+    this.i = i;
+    this.j = j;
     this.x = 0;
     this.v = 0;
+    this.om = 0;
     this.k = 0;
     this.neighs = [];
 }
 
+Site.prototype = {
+    set_omega: function (w) {
+        this.om = w;
+        this.k = w * w;
+    }
+}
+
 AndersonModel.prototype = {
-    init: function() {
+    init: function () {
         // Initialise model
-        this.csize = 100.0/(this.size-1)*0.2;
+        this.csize = 100.0 / (this.size - 1) * 0.2;
         // Create nodes
         var matrix = [];
         for (var i = 0; i < this.size; ++i) {
             matrix.push([]);
             for (var j = 0; j < this.size; ++j) {
-                var s = new Site();
+                var s = new Site(i, j);
                 matrix[i].push(s);
             }
         }
@@ -46,13 +58,39 @@ AndersonModel.prototype = {
                     var j2 = mod(j + dn[k][1], this.size);
                     n.neighs.push(matrix[i2][j2]);
                 }
+                // Now assign the frequency
+                n.set_omega(this.omegaAvg + this.omegaSpread * 2 * (Math.random() - 0.5));
                 this.sites.push(n);
             }
         }
+
+        // Create the visualisation
+        var spacing = 100.0 / (this.size - 1);
+        var cscale = this.cscale;
+        var oA = this.omegaAvg;
+        var oS = this.omegaSpread;
+        var sel = this.cont.selectAll("circle").data(this.sites)
+            .enter()
+            .append("circle")
+            .attr('cx', function (d) {
+                return d.i * spacing - 50;
+            })
+            .attr('cy', function (d) {
+                return d.j * spacing - 50;
+            })
+            .attr('r', this.csize)
+            .attr('fill', function (d) {
+                return cscale((d.om-oA)/oS+0.5).hex();
+            });
+    },
+
+    restart: function () {
+        // Just an alias
+        this.init();
     }
 }
 
-var model = new AndersonModel();
+var model = new AndersonModel('#main_plot');
 
 
 var gui = new dat.GUI();
@@ -60,7 +98,8 @@ var csize = gui.add(model, 'size', 5, 15).step(1);
 var cJ = gui.add(model, 'J', 0, 5.0);
 var cOmAvg = gui.add(model, 'omegaAvg', 1.0, 10.0);
 var cOmSpr = gui.add(model, 'omegaSpread', 1.0, 5.0);
+gui.add(model, 'restart');
 
-csize.onFinishChange(function() {
+csize.onFinishChange(function () {
     console.log(model);
 });
